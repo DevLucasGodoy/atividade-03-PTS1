d209b6b5d004b58d05ef383d782e024c09e99d0f
Lucas Godoy <54687280+LucasMoraes04@users.noreply.github.com>
2021-10-19 15:34:33 +0000

    Ajustes de concordância de código

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var contatosRouter = require('./routes/noticia');
+var noticiassRouter = require('./routes/noticias');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
-app.use('/noticia', contatosRouter);
+app.use('/noticia', noticiasRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/noticias.js
+++ routes/noticias.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('noticia/index', { title: 'Express' });
 });
 
+router.get('/01', function(req, res, next) {
+  res.render('noticia/index2', { title: 'Express' });
+});
+
 module.exports = router;
